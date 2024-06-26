f483c5db0dacd0ce91e84c1c3fc50dfeb4bbbb6f
Denis Kaynar <kaynar.denis@gmail.com>
2024-06-26 16:16:56 +0300

    plugins/sqlite-lua: init

--- plugins/default.nix
+++ plugins/default.nix
@@ -208,6 +208,7 @@
     ./utils/smart-splits.nix
     ./utils/spectre.nix
     ./utils/spider.nix
+    ./utils/sqlite-lua.nix
     ./utils/startify
     ./utils/startup.nix
     ./utils/surround.nix
